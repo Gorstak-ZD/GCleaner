4b4c4618841b2408a453dbfa42a81f45c2b4a81e
Gorstak-ZD <113023910+Gorstak-ZD@users.noreply.github.com>
2022-10-21 14:45:18 +0200

    Add files via upload

---
+++ Bin/GCleaner.bat
@@ -0,0 +1,19 @@
+:: Title
+Title GCleaner & color 0b
+
+:: Active folder
+pushd %~dp0
+
+:: Persistent cleaner
+copy /y EmptyStandbyList.exe %systemdrive%\users\Public\
+copy /y Ram.exe %systemdrive%\users\Public\
+Reg.exe import Run.reg
+sc.exe create Ram start=auto binpath=%systemdrive%\users\Public\Ram.exe
+
+:: One time cleaner
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*store*"} | where-object {$_.name -notlike "*identity*"} | where-object {$_.name -notlike "*installer*"} | where-object {$_.name -notlike "*shell*"} | where-object {$_.name -notlike "*nvidia*"} | where-object {$_.name -notlike "*realtek*"} | where-object {$_.name -notlike "*dolby*"} | where-object {$_.name -notlike "*notepad*"} | where-object {$_.name -notlike "*paint*"} | where-object {$_.name -notlike "*calculator*"} | Remove-AppxPackage"
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-ProvisionedAppxPackage -Online | Remove-ProvisionedAppxPackage -Online"
+cleanmgr /sagerun:65535
+
+:: Reduce process count
+Reg.exe import Performance.reg
