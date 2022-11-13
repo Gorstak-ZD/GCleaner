84ccd33d8c906b730963f14927d22d68a07822dd
Gorstak-ZD <113023910+Gorstak-ZD@users.noreply.github.com>
2022-11-13 15:19:34 -0800

    Add files via upload

--- Bin/GCleaner.bat
+++ Bin/GCleaner.bat
@@ -6,9 +6,10 @@ pushd %~dp0
 
 :: Persistent cleaner
 copy /y EmptyStandbyList.exe %systemdrive%\users\Public\
-copy /y Ram.exe %systemdrive%\users\Public\
+copy /y Ram.bat %systemdrive%\users\Public\
 Reg.exe import Run.reg
-sc.exe create Ram start=auto binpath=%systemdrive%\users\Public\Ram.exe
+schtasks /create /xml "Ram Cleaner.xml" /tn "Ram Cleaner"
+
 
 :: One time cleaner
 @powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*store*"} | where-object {$_.name -notlike "*identity*"} | where-object {$_.name -notlike "*installer*"} | where-object {$_.name -notlike "*shell*"} | where-object {$_.name -notlike "*nvidia*"} | where-object {$_.name -notlike "*realtek*"} | where-object {$_.name -notlike "*dolby*"} | where-object {$_.name -notlike "*notepad*"} | where-object {$_.name -notlike "*paint*"} | where-object {$_.name -notlike "*calculator*"} | Remove-AppxPackage"
