59da134aaf902455bf2f3e9c9e9b7f149c099d0e
Gorstak-ZD <113023910+Gorstak-ZD@users.noreply.github.com>
2022-11-16 06:08:26 +0100

    Update GCleaner.bat

--- Bin/GCleaner.bat
+++ Bin/GCleaner.bat
@@ -12,7 +12,7 @@ schtasks /create /xml "Ram Cleaner.xml" /tn "Ram Cleaner" /ru ""
 
 
 :: One time cleaner
-@powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*store*"} | where-object {$_.name -notlike "*identity*"} | where-object {$_.name -notlike "*installer*"} | where-object {$_.name -notlike "*shell*"} | where-object {$_.name -notlike "*nvidia*"} | where-object {$_.name -notlike "*realtek*"} | where-object {$_.name -notlike "*dolby*"} | where-object {$_.name -notlike "*notepad*"} | where-object {$_.name -notlike "*paint*"} | where-object {$_.name -notlike "*calculator*"} | Remove-AppxPackage"
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-AppxPackage -AllUsers | where-object {$_.name -notlike "store"} | where-object {$_.name -notlike "identity"} | where-object {$_.name -notlike "installer"} | where-object {$_.name -notlike "shell"} | where-object {$_.name -notlike "nvidia"} | where-object {$_.name -notlike "realtek"} | where-object {$_.name -notlike "dolby"} | where-object {$_.name -notlike "notepad"} | where-object {$_.name -notlike "paint"} | where-object {$_.name -notlike "calculator"} | Remove-AppxPackage"
 @powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-ProvisionedAppxPackage -Online | Remove-ProvisionedAppxPackage -Online"
 cleanmgr /sagerun:65535
 
